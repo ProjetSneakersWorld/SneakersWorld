416646dd7ce2855aeb92bd4f4332e8612f0bd00f
me <@.com>
2024-02-14 16:27:05 +0100

    V3.6 add css not finish

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,26 @@ import Head from "next/head";
 import React from "react";
 
 
+import {useState} from 'react';
+import Modal from 'react-modal';
+
 function Index() {
     const router = useRouter();
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
     const handleClick = (route) => {
         router.push(route);
     };
 
-    return (
-        <div>
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+
+    return (<div>
             <Head>
                 <title>Sneakers World</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
@@ -26,23 +37,60 @@ function Index() {
                 </div>
             </div>
             <div className="copyrightContainer">
-                <p>&copy; Projet SAE 2024</p>
-                <p>Mentions legales</p>
+                <p>© Projet SAE 2024</p>
+                <div>
+                    <p onClick={openModal}> Politique de confidentialité</p>
+                </div>
             </div>
-        </div>
-
-    // Conditions Générales d'Utilisation
-    //
-    // Bienvenue chez Sneaker World ! Notre plateforme vous offre la possibilité de vous connecter à une base de données. En utilisant nos services, vous acceptez les présentes Conditions Générales d'Utilisation.
-    // Données personnelles : Nous ne collectons aucune donnée personnelle, hormis les informations requises pour la connexion et la gestion de votre compte. Nous ne vendons ni ne partageons ces informations avec des tiers.
-    //     Base de données : Nous conservons les informations de connexion des utilisateurs dans une base de données exclusivement utilisée pour les besoins fonctionnels de Sneaker World.
-    //     Politique de Confidentialité
-    //
-    // Chez Sneaker World, la protection de vos données personnelles est cruciale. Nous prenons des précautions pour protéger vos informations contre la perte, l'exploitation abusive et l'altération.
-    //     Collecte de données : Nous recueillons les informations de connexion des utilisateurs pour fournir efficacement nos services. Aucune donnée personnelle supplémentaire n'est collectée au-delà de cet objectif.
-    // Partage des données : Dans aucun cas nous ne partageons ni ne vendons les données utilisateur à des tiers, exception faite d'une injonction contraignante des autorités chargées de l'application de la loi.
-    //     Accès aux données : Sur demande, les utilisateurs ont le droit d'accéder, de corriger ou de supprimer les données personnelles stockées.
-    );
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Politique de confidentialité"
+                className="modalPage"
+            >
+                <h2>Politique de confidentialité</h2>
+                <div>
+                    <p>
+                        Bienvenue chez Sneaker World !
+                    </p>
+                    <p>
+                        Notre plateforme vous offre la possibilité de vous connecter à une base de données.</p>
+                    <p>En utilisant nos services, vous acceptez les présentes Conditions Générales
+                        d'Utilisation.
+                    </p>
+                    <p>
+                        Données personnelles : Nous ne collectons aucune donnée personnelle, hormis les informations
+                        requises pour la connexion et la gestion de votre compte. Nous ne vendons ni ne partageons ces
+                        informations avec des tiers.
+                        </p>
+                    <p>Base de données : Nous conservons les informations de connexion des utilisateurs dans une base
+                        de données exclusivement utilisée pour les besoins fonctionnels de Sneaker World.
+                        Politique de Confidentialité
+                    </p>
+                    <p>
+                        Chez Sneaker World, la protection de vos données personnelles est cruciale. Nous prenons des
+                        précautions pour protéger vos informations contre la perte, l'exploitation abusive et
+                        l'altération.
+                    </p>
+                    <p>
+                        Collecte de données : Nous recueillons les informations de connexion des utilisateurs pour
+                        fournir efficacement nos services. Aucune donnée personnelle supplémentaire n'est collectée
+                        au-delà de cet objectif.
+                    </p>
+                    <p>
+                        Partage des données : Dans aucun cas nous ne partageons ni ne vendons les données utilisateur à
+                        des tiers, exception faite d'une injonction contraignante des autorités chargées de
+                        l'application de la loi.
+                    </p>
+                    <p>
+                        Accès aux données : Sur demande, les utilisateurs ont le droit d'accéder, de corriger ou de
+                        supprimer les données personnelles stockées.
+                    </p>
+                </div>
+                <button onClick={closeModal}>Fermer</button>
+            </Modal>
+        </div>);
 }
 
 export default Index;
+
