be76318dfa2fe281849996b951833509d11580be
belbaz <benjam.elbaz@gmail.com>
2024-02-16 16:50:47 +0100

    V7.4 resolve bug

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import {useRouter} from "next/router";
+import {useState} from 'react';
 import "../public/index.css"
 import "../public/style.css"
 import Head from "next/head";
-import React from "react";
-
-
-import {useState} from 'react';
 import Modal from 'react-modal';
+
+Modal.setAppElement('#__next');
 
 function Index() {
     const router = useRouter();
@@ -25,71 +24,71 @@ function Index() {
     };
 
     return (<div>
-            <Head>
-                <title>Sneakers World</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-            </Head>
-            <div className="body2">
-                <div className="welcomDiv">
-                    <h1 className="h1">Welcome to Sneakers World</h1>
-                    <button className="button" onClick={() => handleClick('/login')}>Login</button>
-                    <button className="button" onClick={() => handleClick('/signup')}>Signup</button>
-                </div>
+        <Head>
+            <title>Sneakers World</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        </Head>
+        <div className="body2">
+            <div className="welcomDiv">
+                <h1 className="h1">Welcome to Sneakers World</h1>
+                <button className="button" onClick={() => handleClick('/login')}>Login</button>
+                <button className="button" onClick={() => handleClick('/signup')}>Signup</button>
             </div>
-            <div className="copyrightContainer">
-                <p>© Projet SAE 2024</p>
-                <div>
-                    <p onClick={openModal}> Politique de confidentialité</p>
-                </div>
+        </div>
+        <div className="copyrightContainer">
+            <p>© Projet SAE 2024</p>
+            <div>
+                <p onClick={openModal}> Politique de confidentialité</p>
             </div>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                contentLabel="Politique de confidentialité"
-                className="modalPage"
-            >
-                <h2>Politique de confidentialité</h2>
-                <div>
-                    <p>
-                        Bienvenue chez Sneaker World !
-                    </p>
-                    <p>
-                        Notre plateforme vous offre la possibilité de vous connecter à une base de données.</p>
-                    <p>En utilisant nos services, vous acceptez les présentes Conditions Générales
-                        d'Utilisation.
-                    </p>
-                    <p>
-                        Données personnelles : Nous ne collectons aucune donnée personnelle, hormis les informations
-                        requises pour la connexion et la gestion de votre compte. Nous ne vendons ni ne partageons ces
-                        informations avec des tiers.
-                        </p>
-                    <p>Base de données : Nous conservons les informations de connexion des utilisateurs dans une base
-                        de données exclusivement utilisée pour les besoins fonctionnels de Sneaker World.
-                        Politique de Confidentialité
-                    </p>
-                    <p>
-                        Chez Sneaker World, la protection de vos données personnelles est cruciale. Nous prenons des
-                        précautions pour protéger vos informations contre la perte, l'exploitation abusive et
-                        l'altération.
-                    </p>
-                    <p>
-                        Collecte de données : Nous recueillons les informations de connexion des utilisateurs pour
-                        fournir efficacement nos services. Aucune donnée personnelle supplémentaire n'est collectée
-                        au-delà de cet objectif.
-                    </p>
-                    <p>
-                        Partage des données : Dans aucun cas nous ne partageons ni ne vendons les données utilisateur à
-                        des tiers, exception faite d'une injonction contraignante des autorités chargées de
-                        l'application de la loi.
-                    </p>
-                    <p>
-                        Accès aux données : Sur demande, les utilisateurs ont le droit d'accéder, de corriger ou de
-                        supprimer les données personnelles stockées.
-                    </p>
-                </div>
-                <button onClick={closeModal}>Fermer</button>
-            </Modal>
-        </div>);
+        </div>
+        <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Politique de confidentialité"
+            className="modalPage"
+        >
+            <h2>Politique de confidentialité</h2>
+            <div>
+                <p>
+                    Bienvenue chez Sneaker World !
+                </p>
+                <p>
+                    Notre plateforme vous offre la possibilité de vous connecter à une base de données.</p>
+                <p>En utilisant nos services, vous acceptez les présentes Conditions Générales
+                    d'Utilisation.
+                </p>
+                <p>
+                    Données personnelles : Nous ne collectons aucune donnée personnelle, hormis les informations
+                    requises pour la connexion et la gestion de votre compte. Nous ne vendons ni ne partageons ces
+                    informations avec des tiers.
+                </p>
+                <p>Base de données : Nous conservons les informations de connexion des utilisateurs dans une base
+                    de données exclusivement utilisée pour les besoins fonctionnels de Sneaker World.
+                    Politique de Confidentialité
+                </p>
+                <p>
+                    Chez Sneaker World, la protection de vos données personnelles est cruciale. Nous prenons des
+                    précautions pour protéger vos informations contre la perte, l'exploitation abusive et
+                    l'altération.
+                </p>
+                <p>
+                    Collecte de données : Nous recueillons les informations de connexion des utilisateurs pour
+                    fournir efficacement nos services. Aucune donnée personnelle supplémentaire n'est collectée
+                    au-delà de cet objectif.
+                </p>
+                <p>
+                    Partage des données : Dans aucun cas nous ne partageons ni ne vendons les données utilisateur à
+                    des tiers, exception faite d'une injonction contraignante des autorités chargées de
+                    l'application de la loi.
+                </p>
+                <p>
+                    Accès aux données : Sur demande, les utilisateurs ont le droit d'accéder, de corriger ou de
+                    supprimer les données personnelles stockées.
+                </p>
+            </div>
+            <button className="button" style={{marginTop: "15px"}} onClick={closeModal}>Fermer</button>
+        </Modal>
+    </div>);
 }
 
 export default Index;
--- src/scenes/Scene.js
+++ src/scenes/Scene.js
@@ -69,7 +69,6 @@ function Scene() {
                 colision.addCollidesWith(this.player);
                 colision.setCollisionByExclusion([-1]);
                 this.physics.add.collider(this.player, colision, this.handleCollision, null, this);
-                // Align.scaleToGameW(this.player,0.15,this)
 
                 this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
                 this.cameras.main.startFollow(this.player);
@@ -86,10 +85,9 @@ function Scene() {
                     this.game.canvas.style.border = "5px solid white";
                     this.game.canvas.style.borderRadius = "15px";
                 }else{
-                    this.game.canvas.style.border = "7px solid red";
+                    this.game.canvas.style.border = "5px solid green";
                     this.game.canvas.style.borderRadius = "15px";
                 }
-
 
                 this.player.setVelocity(0);
 
@@ -112,89 +110,25 @@ function Scene() {
             }
 
             handleCollision(player, collisionLayer) {
-                if (Math.abs(this.player.x >= 912) && Math.abs(this.player.x <= 974)
-                    && Math.abs(this.player.y >= 1177) && Math.abs(this.player.y <= 1208)) {
-                    // Charger la nouvelle carte
-                    console.log("ENTRER dans SceneShop");
-                    this.scene.start('SceneShop'); // Changez à la scène 'SceneShop'
-                    setCurrentScene('SceneShop');
+                if(currentScene===''){
+                    if (Math.abs(this.player.x >= 912) && Math.abs(this.player.x <= 974)
+                        && Math.abs(this.player.y >= 1177) && Math.abs(this.player.y <= 1208)) {
+                        // Charger la nouvelle carte
+                        console.log("ENTRER dans SceneShop");
+                        this.scene.start('SceneShop'); // Changez à la scène 'SceneShop'
+                        setCurrentScene('SceneShop');
+                    }
                 }
             }
 
         }
 
-
-        // class SceneShop extends Phaser.Scene {
-        //     constructor() {
-        //         super("SceneShop");
-        //     }
-        //
-        //     preload() {
-        //         this.load.image("tiles","/assets/store.png");
-        //         this.load.tilemapTiledJSON('map',"/assets/shopMapOthman.json");
-        //         this.load.spritesheet('perso', '/assets/perso.png', { frameWidth: 32, frameHeight: 32 });
-        //     }
-        //
-        //     create() {
-        //         const map = this.make.tilemap({ key: "map", tileWidth: 32, tileHeight: 32});
-        //
-        //         const tileset = map.addTilesetImage("tiles","tiles");
-        //         const layer = map.createLayer("void", tileset, 0, 0);
-        //         const colision = map.createLayer("colision",tileset,0,0)
-        //         this.player = this.physics.add.sprite(437,844,"perso");
-        //
-        //
-        //         // colision.setCollisionByProperty({ collideBottom: true });
-        //         // colision.addCollidesWith(this.player);
-        //         // colision.setCollisionByExclusion([-1]);
-        //         // this.physics.add.collider(this.player, colision, this.handleCollision, null, this);
-        //         // Align.scaleToGameW(this.player,0.15,this)
-        //
-        //         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-        //         this.cameras.main.startFollow(this.player);
-        //         this.physics.add.collider(this.player, colision);
-        //         this.cameras.main.setFollowOffset(-100, -100);
-        //         this.player.setScale(1.5);
-        //         this.cursors = this.input.keyboard.createCursorKeys();
-        //     }
-        //
-        //     update = () => {
-        //         // console.log("X: "+this.player.x + " Y:"+ this.player.y)
-        //         // console.log(isInputFocused)
-        //         this.game.canvas.style.border = "5px solid white";
-        //         this.game.canvas.style.borderRadius = "15px";
-        //
-        //         this.player.setVelocity(0);
-        //
-        //         if (this.cursors.up.isDown) {
-        //             this.player.setVelocityY(-speed);
-        //             this.player.anims.play('up', true);
-        //         } else if (this.cursors.left.isDown) {
-        //             this.player.setVelocityX(-speed);
-        //             this.player.anims.play('left', true);
-        //         } else if (this.cursors.right.isDown) {
-        //             this.player.setVelocityX(speed);
-        //             this.player.anims.play('right', true);
-        //         } else if (this.cursors.down.isDown) {
-        //             this.player.setVelocityY(speed);
-        //             this.player.anims.play('down', true);
-        //         } else {
-        //             // If no cursor is down, stop the animation
-        //             this.player.anims.stop();
-        //         }
-        //     }
-        //     handleCollision(player, collisionLayer) {
-        //
-        //     }
-        // }
-
-
         const config = {
             type: Phaser.AUTO,
             parent: gameContainer.current,
-            width: "75%",
-            height: "84%",
-            scene: [SceneMain], // Utilisez un tableau pour la scène
+            width: window.innerWidth-700,
+            height: window.innerHeight-180,
+            scene: [SceneMain],
             audio: {
                 disableWebAudio: true,
             },
@@ -205,6 +139,7 @@ function Scene() {
                 }
             },
         };
+
 
 
         const game = new Phaser.Game(config);
